91237b303d229d114c3bc642b87210aa2aded860
Muhammed Bacalan <muhammedbacalan@gmail.com>
2022-01-12 17:49:22 +0300

    Improve poll comp. with preview message and sortable options

--- components/commands/poll.vue
+++ components/commands/poll.vue
@@ -9,6 +9,7 @@
             hide-details="auto"
             outlined
             dense
+            required
           />
         </v-col>
       </v-row>
@@ -21,27 +22,68 @@
             hide-details="auto"
             outlined
             dense
+            required
           />
         </v-col>
 
         <v-col cols="12" sm="6">
-          <v-btn type="submit" color="primary" @click.prevent="addPollOption">
+          <v-btn type="submit" color="primary" @click.prevent="addOption">
             Add
           </v-btn>
         </v-col>
       </v-row>
 
       <v-row v-if="options.length">
-        <v-col cols="6">
-          <div v-for="(option, index) in options" :key="index" class="d-flex">
-            <p class="mr-auto">
-              {{ index + 1 }} - {{ option }}
-            </p>
+        <v-col cols="12" sm="6">
+          <transition-group name="poll-options" tag="ul">
+            <li v-for="(option, index) in options" :key="option" class="d-flex">
+              <p class="mr-auto">
+                {{ pollEmojiUnicodes[index] }} - {{ option }}
+              </p>
 
-            <v-icon @click="deletePollOption(index)">
-              mdi-close
-            </v-icon>
-          </div>
+              <v-icon
+                :disabled="index == 0"
+                color="primary"
+                @click="moveOption('up', index)"
+              >
+                mdi-arrow-up-box
+              </v-icon>
+
+              <v-icon
+                :disabled="index == options.length - 1"
+                color="primary"
+                @click="moveOption('down', index)"
+              >
+                mdi-arrow-down-box
+              </v-icon>
+
+              <v-icon
+                color="primary"
+                @click="deleteOption(index)"
+              >
+                mdi-close-box
+              </v-icon>
+            </li>
+          </transition-group>
+        </v-col>
+      </v-row>
+
+      <v-row v-if="options.length">
+        <v-col cols="12" sm="6">
+          <discord-messages>
+            <discord-message
+              author="AureneBot"
+              :bot="true"
+              avatar="https://cdn.discordapp.com/avatars/400644240746479616/9bcbf1034b7db9cbfc4951eb46ffe9f4.webp?size=128"
+            >
+              <mention>everyone</mention>
+              <discord-embed slot="embeds" :title="title">
+                <p v-for="(option, index) in options" :key="option" class="mb-2">
+                  {{ pollEmojiUnicodes[index] }} {{ option }}
+                </p>
+              </discord-embed>
+            </discord-message>
+          </discord-messages>
         </v-col>
       </v-row>
 
@@ -49,6 +91,10 @@
         <v-col cols="12">
           <v-btn type="submit" color="primary" @click.prevent="createPoll">
             Create Poll
+          </v-btn>
+
+          <v-btn v-if="isFormDirty" type="submit" color="warning" @click="clearForm">
+            Clear Form
           </v-btn>
         </v-col>
       </v-row>
@@ -58,6 +104,7 @@
 
 <script>
 import { mapState } from 'vuex'
+import { pollEmojiUnicodes } from '~/api/data/emoji'
 
 export default {
   name: 'Poll',
@@ -65,14 +112,18 @@ export default {
     return {
       title: '',
       newOption: '',
-      options: []
+      options: [],
+      pollEmojiUnicodes
     }
   },
   computed: {
-    ...mapState(['guild'])
+    ...mapState(['guild']),
+    isFormDirty () {
+      return !!((this.title.length || this.options.length))
+    }
   },
   methods: {
-    addPollOption () {
+    addOption () {
       if (!this.newOption) {
         return
       }
@@ -80,8 +131,24 @@ export default {
       this.options.push(this.newOption)
       this.newOption = ''
     },
-    deletePollOption (index) {
+    moveOption (direction, index) {
+      this.options = this.options.map((element, i) => {
+        if (direction == 'up' ? i == index - 1 : i == index) {
+          return this.options[direction == 'up' ? index : index + 1]
+        } else if (direction == 'up' ? i == index : i == index + 1) {
+          return this.options[direction == 'up' ? index - 1 : index]
+        } else {
+          return element
+        }
+      })
+    },
+    deleteOption (index) {
       this.options.splice(index, 1)
+    },
+    clearForm () {
+      this.title = ''
+      this.newOption = ''
+      this.options = []
     },
     async createPoll () {
       if (this.options.length <= 1) {
@@ -101,7 +168,15 @@ export default {
       if (response.ok) {
         alert('Success')
       }
+
+      this.clearForm()
     }
   }
 }
 </script>
+
+<style>
+  .poll-options-move {
+    transition: transform 0.3s ease;
+  }
+</style>
